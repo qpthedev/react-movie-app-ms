30734b1144c20a8276db4e615810dbf39a459050
qpthedev <qpthedev@gmail.com>
2020-07-30 05:29:57 -0400

    [Add] Add styled components

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import Header from "Components/Header";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
 
 export default () => (
   <Router>
-    <Route path="/" exact component={Home}></Route>
-    <Route path="/tv" exact component={TV}></Route>
-    <Route path="/search" exact component={Search}></Route>
+    <>
+      <Header></Header>
+      <Switch>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/tv" exact component={TV}></Route>
+        <Route path="/search" component={Search}></Route>
+        <Redirect from="*" to="/"></Redirect>
+      </Switch>
+    </>
   </Router>
 );
