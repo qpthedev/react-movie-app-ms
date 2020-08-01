d9b3bee747b320f61ff097e9d4c1682b23c08ede
qpthedev <qpthedev@gmail.com>
2020-08-01 02:55:23 -0400

    [Add] add dependency - dotenv

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import dotenv from "dotenv";
 import App from "Components/App";
 import "./api";
+
+dotenv.config();
 
 ReactDOM.render(
   <React.StrictMode>
