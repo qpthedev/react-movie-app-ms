41bcce60bc21ecada7f10e3b5a70d63fe1a816d2
qpthedev <qpthedev@gmail.com>
2020-08-08 23:50:05 -0400

    [Add] update router to show show and movie detail

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -5,20 +5,23 @@ import {
   Redirect,
   Switch,
 } from "react-router-dom";
-import Header from "Components/Header";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
+import Header from "Components/Header";
 import Search from "Routes/Search";
+import Detail from "Routes/Detail";
 
 export default () => (
   <Router>
     <>
-      <Header></Header>
+      <Header />
       <Switch>
-        <Route path="/" exact component={Home}></Route>
-        <Route path="/tv" exact component={TV}></Route>
-        <Route path="/search" component={Search}></Route>
-        <Redirect from="*" to="/"></Redirect>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" component={TV} />
+        <Route path="/search" component={Search} />
+        <Route path="/movie/:id" component={Detail} />
+        <Route path="/show/:id" component={Detail} />
+        <Redirect from="*" to="/" />
       </Switch>
     </>
   </Router>
