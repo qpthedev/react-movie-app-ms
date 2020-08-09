0eb566124c8136e1fc7a51fbc54f6b3cf6af6198
qpthedev <qpthedev@gmail.com>
2020-08-09 01:12:04 -0400

    [Add] edit import routes for netlify deploy

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 import styled from "styled-components";
-import Loader from "Components/Loader";
+import Loader from "../../Components/Loader";
 
 const imageBaseUrl = `https://image.tmdb.org/t/p/original`;
 
