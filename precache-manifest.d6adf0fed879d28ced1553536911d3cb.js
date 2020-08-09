b4a39e6e0bcc57e8f532ef5a0302a3e20f3d813c
qpthedev <qpthedev@gmail.com>
2020-08-09 00:39:30 -0400

    Updates

---
+++ precache-manifest.d6adf0fed879d28ced1553536911d3cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce2b763f14cbdd607c158f32aec065af",
+    "url": "/react-movie-app-ms/index.html"
+  },
+  {
+    "revision": "2a9c414cba5b89efeff0",
+    "url": "/react-movie-app-ms/static/js/2.09507f31.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/react-movie-app-ms/static/js/2.09507f31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fca2e55988ed36e0c1f",
+    "url": "/react-movie-app-ms/static/js/main.c5bf59de.chunk.js"
+  },
+  {
+    "revision": "ec4ef3b5d7d06df79371",
+    "url": "/react-movie-app-ms/static/js/runtime-main.40da36eb.js"
+  },
+  {
+    "revision": "6a69071019d6bb7c71c46a152a9120a6",
+    "url": "/react-movie-app-ms/static/media/noPosterSmall.6a690710.png"
+  }
+]);
